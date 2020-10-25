483dec7b800fb4887cce93135827c169c957318f
kiranpe <kiranwls2011@gmail.com>
2020-10-25 13:55:28 +0530

    creating aws vpc using terraform

---
+++ VPC.tf
@@ -0,0 +1,29 @@
+//VPC
+########
+
+locals {
+  vpc_tags = {
+    createdby = "Kiran Peddineni"
+    App       = "vpc-${terraform.workspace}"
+    Approver  = "Kiran Peddineni"
+  }
+}
+
+resource "aws_vpc" "this" {
+  count = var.create_vpc ? 1 : 0
+
+  cidr_block                       = var.cidr
+  instance_tenancy                 = var.instance_tenancy
+  enable_dns_hostnames             = var.enable_dns_hostnames
+  enable_dns_support               = var.enable_dns_support
+  enable_classiclink               = var.enable_classiclink
+  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
+  assign_generated_ipv6_cidr_block = var.enable_ipv6
+
+  tags = merge(
+    {
+      "Name" = "${var.name}-${terraform.workspace}"
+    },
+    local.vpc_tags
+  )
+}
